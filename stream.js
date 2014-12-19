0f01a2e840529a787f30974f1fd7b691d7844d01
Arthur Viegers <arthur@mongodb.com>
2014-12-19 16:39:30 +0100

    Connect to application db

--- stream.js
+++ stream.js
@@ -8,55 +8,60 @@ var db = null;
 
 var args = process.argv.slice(2);
  
-MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], function(err, dbConnection) {
+MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], function(err, aiotaDB) {
 	if (err) {
 		aiota.log(processName, "", null, err);
 	}
 	else {
-		db = dbConnection;
-
-		aiota.getConfig(db, function(c) {
+		aiota.getConfig(aiotaDB, function(c) {
 			if (c == null) {
-				aiota.log(processName, "", db, "Error getting config from database");
+				aiota.log(processName, "", aiotaDB, "Error getting config from database");
 			}
 			else {
 				config = c;
 
-				var port = config.ports["aiota-stream"][0];
-
-				http.createServer(function (request, response) {
-					response.writeHead(200, {
-						"Content-Type": "text/event-stream",
-						"Cache-Control": "no-cache",
-						"Access-Control-Allow-Origin": "*"
-					});
-				
-					db.collection("push_actions", function(err, collection) {
-						if (err) {
-							return;
-						}
-
-						var filter = {};
+				MongoClient.connect("mongodb://" + config.database.host + ":" + config.ports.mongodb + "/" + config.database.name, function(err, dbConnection) {
+					if (err) {
+						aiota.log(processName, config.serverName, aiotaDB, err);
+					}
+					else {
+						db = dbConnection;
 						
-						// Set MongoDB cursor options
-						var cursorOptions = {
-							tailable: true,
-							awaitdata: true,
-							numberOfRetries: -1
-						};
+						http.createServer(function (request, response) {
+							response.writeHead(200, {
+								"Content-Type": "text/event-stream",
+								"Cache-Control": "no-cache",
+								"Access-Control-Allow-Origin": "*"
+							});
 						
-						// Create stream and listen
-						var stream = collection.find(filter, cursorOptions).stream();
+							db.collection("push_actions", function(err, collection) {
+								if (err) {
+									return;
+								}
+		
+								var filter = {};
 								
-						// call the callback
-						stream.on("data", function(doc) {
-							response.write("data: " + JSON.stringify(doc) + "\n\n");
-						});
-					});
-					
-					response.on("close", function () {
-					});
-				}).listen(port);
+								// Set MongoDB cursor options
+								var cursorOptions = {
+									tailable: true,
+									awaitdata: true,
+									numberOfRetries: -1
+								};
+								
+								// Create stream and listen
+								var stream = collection.find(filter, cursorOptions).stream();
+										
+								// call the callback
+								stream.on("data", function(doc) {
+									response.write("data: " + JSON.stringify(doc) + "\n\n");
+								});
+							});
+							
+							response.on("close", function () {
+							});
+						}).listen(port);
+					}
+				});
 			}
 		});
 	}
