1f844fb321099df8bc1e7a0cbe7d05685fa82bf5
Arthur Viegers <arthur@mongodb.com>
2014-12-19 15:52:43 +0100

    Get config from database

--- stream.js
+++ stream.js
@@ -1,24 +1,48 @@
-var PORT = 30030;
-
+var aiota = require("aiota-utils");
 var http = require("http");
-var fs = require("fs");
-var url = require("url");
+var MongoClient = require("mongodb").MongoClient;
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      "Access-Control-Allow-Origin": "*"
-    });
+var config = null;
+var processName = "stream.js";
+var db = null;
 
-	var f = function() {
-        response.write("data: " + Date.now() + "\n\n");
-        timeoutId = setTimeout(f, 1000);
-    };
+var args = process.argv.slice(2);
+ 
+MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], function(err, dbConnection) {
+	if (err) {
+		aiota.log(processName, "", null, err);
+	}
+	else {
+		db = dbConnection;
 
-    f();
+		aiota.getConfig(db, function(c) {
+			if (c == null) {
+				aiota.log(processName, "", db, "Error getting config from database");
+			}
+			else {
+				config = c;
 
-    response.on("close", function () {
-      clearTimeout(timeoutId);
-    });
-}).listen(PORT);
+				var port = config.ports["aiota-stream"][0];
+
+				http.createServer(function (request, response) {
+					response.writeHead(200, {
+						"Content-Type": "text/event-stream",
+						"Cache-Control": "no-cache",
+						"Access-Control-Allow-Origin": "*"
+					});
+				
+					var f = function() {
+						response.write("data: " + Date.now() + "\n\n");
+						timeoutId = setTimeout(f, 1000);
+					};
+				
+					f();
+				
+					response.on("close", function () {
+						clearTimeout(timeoutId);
+					});
+				}).listen(port);
+			}
+		});
+	}
+});
