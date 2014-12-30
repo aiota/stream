80cef173440e27abb2b3a3835fe23d7b22f993f5
Arthur Viegers <arthur@mongodb.com>
2014-12-30 15:47:06 +0100

    Store server name and ip in logs

--- stream.js
+++ stream.js
@@ -1,28 +1,28 @@
 var aiota = require("aiota-utils");
+var path = require("path");
 var http = require("http");
 var MongoClient = require("mongodb").MongoClient;
 
 var config = null;
-var processName = "stream.js";
 var db = null;
 
 var args = process.argv.slice(2);
  
 MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], function(err, aiotaDB) {
 	if (err) {
-		aiota.log(processName, "", null, err);
+		aiota.log(path.basename(__filename), "", null, err);
 	}
 	else {
 		aiota.getConfig(aiotaDB, function(c) {
 			if (c == null) {
-				aiota.log(processName, "", aiotaDB, "Error getting config from database");
+				aiota.log(path.basename(__filename), "", aiotaDB, "Error getting config from database");
 			}
 			else {
 				config = c;
 
 				MongoClient.connect("mongodb://" + config.database.host + ":" + config.ports.mongodb + "/" + config.database.name, function(err, dbConnection) {
 					if (err) {
-						aiota.log(processName, config.serverName, aiotaDB, err);
+						aiota.log(path.basename(__filename), config.server, aiotaDB, err);
 					}
 					else {
 						db = dbConnection;
