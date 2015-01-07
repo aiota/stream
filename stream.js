9db8a6c48095647d93d6a3a0359828ea594e1960
Arthur Viegers <arthur@mongodb.com>
2015-01-07 14:02:40 +0100

    Make direct AMQP rpc call to longpolling process

--- stream.js
+++ stream.js
@@ -2,10 +2,11 @@ var aiota = require("aiota-utils");
 var path = require("path");
 var http = require("http");
 var url = require("url");
+var amqprpc = require("amqp-rpc");
 var MongoClient = require("mongodb").MongoClient;
 
 var config = null;
-var db = null;
+//var db = null;
 
 var args = process.argv.slice(2);
  
@@ -21,25 +22,49 @@ MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], func
 			else {
 				config = c;
 
+				var rpc = amqprpc.factory({ url: "amqp://" + config.amqp.login + ":" + config.amqp.password + "@" + config.amqp.host + ":" + config.amqp.port });
+/*
 				MongoClient.connect("mongodb://" + config.database.host + ":" + config.ports.mongodb + "/" + config.database.name, function(err, dbConnection) {
 					if (err) {
 						aiota.log(path.basename(__filename), config.server, aiotaDB, err);
 					}
 					else {
 						db = dbConnection;
-						
+*/						
 						var port = config.ports["aiota-stream"][0];
 							 
 						http.createServer(function (request, response) {
 							var queryData = url.parse(request.url, true).query;
 
-							if (queryData.hasOwnProperty("deviceId")) {
+							if (queryData.hasOwnProperty("deviceId") && queryData.hasOwnProperty("tokencardId") {
 								response.writeHead(200, {
 									"Content-Type": "text/event-stream",
 									"Cache-Control": "no-cache",
 									"Access-Control-Allow-Origin": "*"
 								});
 							
+								var obj = {
+									header: {
+										requestId: "testReqId",
+										deviceId: queryData.deviceId,
+										type: "poll",
+										timestamp: Date.now(),
+										ttl: 86400,
+										encryption: {
+											method: "aes-256-gcm",
+											tokencardId: queryData.tokencardId
+										}
+									},
+									body: { timeout: 0 },
+									nonce: 0
+								};
+
+								rpc.call("longpolling-queue", obj, function(result) {
+									console.log(result);
+									response.write("data: " + JSON.stringify(result) + "\n\n");
+								});
+
+/*
 								db.collection("applications", function(err, collection) {
 									if (err) {
 //										callback({ error: err, errorCode: 200001 });
@@ -56,70 +81,80 @@ MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], func
 												if (appl.hasOwnProperty("tokens")) {
 													db.collection("push_actions", function(err, collection) {
 														if (err) {
+//															callback({ error: err, errorCode: 200001 });
 															return;
 														}
 								
-														var filter = { deviceId: queryData.deviceId, "encryption.tokencardId": queryData.tokencardId, status: { $lt: 10 } };
-														
-														// Set MongoDB cursor options
-														var cursorOptions = {
-															tailable: true,
-															awaitdata: true,
-															numberOfRetries: -1
-														};
-														
-														// Create stream and listen
-														var stream = collection.find(filter, cursorOptions).stream();
-																
-														// call the callback
-														stream.on("data", function(doc) {
-															var now = Date.now();
-															var sendToDevice = true;
-															
-															if (doc.timeoutAt <= now) {
-																// This action has timed out
-																// actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "timed out" }, status: 30 });
-																sendToDevice = false;
+														// First we insert a dummy push action to guarantee that the tailable cursor returns a valid cursor
+														collection.insert({ deviceId: queryData.deviceId, encryption: { method: "none", tokencardId: queryData.tokencardId }, status: 0, timeoutAt: 0 }, function(err, result) {
+															if (err) {
+//																callback({ error: err, errorCode: 200002 });
+																return;
 															}
-															else {
-																if (doc.status == 0) {
-																	//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "sent to device" }, status: 1, resendAfter: now + actions[i].resends.resendTimeout });
+											
+															// Now create a tailable cursor for this device				
+															var filter = { deviceId: queryData.deviceId, "encryption.tokencardId": queryData.tokencardId, status: { $lt: 10 } };
+															
+															// Set MongoDB cursor options
+															var cursorOptions = {
+																tailable: true,
+																awaitdata: true,
+																numberOfRetries: -1
+															};
+															
+															// Create stream and listen
+															var stream = collection.find(filter, cursorOptions).stream();
+																	
+															// call the callback
+															stream.on("data", function(doc) {
+																var now = Date.now();
+																var sendToDevice = true;
+																
+																if (doc.timeoutAt <= now) {
+																	// This action has timed out
+																	// actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "timed out" }, status: 30 });
+																	sendToDevice = false;
 																}
 																else {
-																	// This is a resend
-																	if (doc.resends.numResends < doc.resends.maxResends) {
-																		if (doc.resends.resendAfter <= now) {
-																			// The resend timeout has expired
-																			//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "resent to device" }, status: 2, resendAfter: now + actions[i].resends.resendTimeout });
+																	if (doc.status == 0) {
+																		//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "sent to device" }, status: 1, resendAfter: now + actions[i].resends.resendTimeout });
+																	}
+																	else {
+																		// This is a resend
+																		if (doc.resends.numResends < doc.resends.maxResends) {
+																			if (doc.resends.resendAfter <= now) {
+																				// The resend timeout has expired
+																				//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "resent to device" }, status: 2, resendAfter: now + actions[i].resends.resendTimeout });
+																			}
+																			else {
+																				// Don't send this action until the resend timeout expires
+																				sendToDevice = false;
+																			}
 																		}
 																		else {
-																			// Don't send this action until the resend timeout expires
+																			// We have exhausted the maximum number of resends
+																			//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "max. resends exhausted" }, status: 31 });
 																			sendToDevice = false;
 																		}
 																	}
-																	else {
-																		// We have exhausted the maximum number of resends
-																		//actionUpdateQueue.push({ _id: actions[i]["_id"].toString(), progress: { timestamp: now, status: "max. resends exhausted" }, status: 31 });
-																		sendToDevice = false;
+																}
+											
+																if (sendToDevice) {
+																	var action = { action: doc.action, requestId: doc.requestId };
+																	if (doc.hasOwnProperty("params")) {
+																		action["params"] = doc.params;
 																	}
+																	
+																	var nonce = 0;
+						
+																	aiota.respond(null, queryData.deviceId, { group: "response", type: "stream" }, doc.encryption.method, queryData.tokencardId, appl.tokens, nonce, action, function(msg) {
+																		response.write("data: " + JSON.stringify(msg) + "\n\n");
+																	});
+						
+																	//process.nextTick(updateActions);		
 																}
-															}
-										
-															if (sendToDevice) {
-																var action = { action: doc.action, requestId: doc.requestId };
-																if (doc.hasOwnProperty("params")) {
-																	action["params"] = doc.params;
-																}
-																
-																var nonce = 0;
-					
-																aiota.respond(null, queryData.deviceId, { group: "response", type: "stream" }, doc.encryption.method, queryData.tokencardId, appl.tokens, nonce, action, function(msg) {
-																	response.write("data: " + JSON.stringify(msg) + "\n\n");
-																});
-					
-																//process.nextTick(updateActions);		
-															}
-														});												
+															});												
+															});
 													});
 												}
 												else {
@@ -132,13 +167,23 @@ MongoClient.connect("mongodb://" + args[0] + ":" + args[1] + "/" + args[2], func
 										}
 									});
 								});
-								
+*/								
 								response.on("close", function () {
 								});
 							}
 						}).listen(port);
+
+						setInterval(function() { aiota.heartbeat(path.basename(__filename), config.server, aiotaDB); }, 10000);
+		
+						process.on("SIGTERM", function() {
+							aiota.terminateProcess(path.basename(__filename), config.server, aiotaDB, function() {
+								process.exit(1);
+							});
+						});
+/*
 					}
 				});
+*/
 			}
 		});
 	}
